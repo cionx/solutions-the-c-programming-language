b2a359b7c7bc2f880608ae11cc0990f020451c1b
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-09 20:04:23 +0200

    Add a solution to Exercise 1-17.

---
+++ chapter_1_a_tutorial_introduction/exercise_1-17.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define MAXLINE 1000 /* maximum input line size */
+
+int getaline(char line[], int maxline);
+
+/* print longest input line */
+int main(void)
+{
+	int len;                /* current line length */
+	char line[MAXLINE];     /* current input line */
+
+	while ((len = getaline(line, MAXLINE)) > 0)
+		if (len > 80)
+			printf("%s", line);
+	return 0;
+}
+
+/* getaline: read a line into s, return length */
+int getaline(char s[], int lim)
+{
+	int c, i;
+	
+	c = 0;
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
